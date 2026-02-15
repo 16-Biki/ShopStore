6010bad4b666b10ffb2c150d7c501c6538f447f2
Bikram Mahaldar <127871664+16-Biki@users.noreply.github.com>
2026-02-15 17:06:45 +0530

    Update API base URL to use dynamic configuration

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:5000/api"
+    : "https://shopstore-v706.onrender.com/api";
+
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
