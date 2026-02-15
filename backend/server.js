3afbe5acac23eff2d4636f9af858dc5a62b48f95
Bikram Mahaldar <127871664+16-Biki@users.noreply.github.com>
2026-02-15 17:23:28 +0530

    Refactor server.js for CORS and server startup

--- backend/server.js
+++ backend/server.js
@@ -10,23 +10,33 @@ const orderRoutes = require("./routes/orderRoutes");
 
 const app = express();
 
+/* BODY PARSER */
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(cookieParser());
 
+/* CORS CONFIG */
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://shop-store-eta.vercel.app",
+    ],
     credentials: true,
-  }),
+  })
 );
 
+/* ROUTES */
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
+/* DB CONNECT */
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("Mongo Connected"));
+  .then(() => console.log("Mongo Connected"))
+  .catch((err) => console.log(err));
 
-app.listen(5000, () => console.log("Server running 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("Server running on", PORT));
