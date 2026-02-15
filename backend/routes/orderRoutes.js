4731f8cb6d1565bca19213c4415f7c0ee4c2c8fd
Bikram Mahaldar <127871664+16-Biki@users.noreply.github.com>
2026-02-15 18:16:04 +0530

    Remove comment about delete button in order routes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -12,7 +12,6 @@ const admin = require("../middlewares/admin");
 router.post("/", auth, createOrder);
 router.get("/", auth, admin, getAllOrders);
 
-// 🔥 ONE BUTTON → DELETE
 router.put("/complete/:id", auth, admin, completeOrder);
 
 module.exports = router;
